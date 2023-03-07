d3410d4602264cd732a407e439237ddf0f097d91
jens8n <jensen.bucoy@gmail.com>
2023-03-07 18:54:08 +0400

    check for leaks in codespace

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jebucoy <jebucoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 18:59:34 by jebucoy           #+#    #+#             */
-/*   Updated: 2023/03/07 17:10:08 by jebucoy          ###   ########.fr       */
+/*   Updated: 2023/03/07 18:52:56 by jebucoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,58 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+int	parser(t_stack *stack)
+{
+	if (check_sort(stack) == true)
+	{
+		printf("ERROR: Args already sorted\n");
+		return (false);
+	}
+	if (check_dup(stack) == true)
+	{
+		printf("ERROR: Duplicates in argument\n");
+		return (false);
+	}
+	return (true);
+}
+
 int	push_swap(char **av)
 {
 	t_data	*data;
 
 	data = init_struct(av);
-	printf("list size: %zd\n", ps_lst_size(data->a));
-	if (check_sort(data->a) == true)
-	{
-		printf("ERROR: Args already sorted\n");
-		exit (0);
-	}
-	if (check_dup(data->a) == true)
-	{
-		printf("ERROR: Duplicates in argument\n");
-		exit (0);
-	}
-	char	line[256];
-	while (1) {
-		scanf("%255[^\n]%*c",line);
-		if (ft_strcmp(line, "sa") == 0)
-			swap_a(data, 1);
-		else if (ft_strcmp(line, "sb") == 0)
-			swap_b(data, 1);
-		else if (ft_strcmp(line, "ss") == 0)
-			sa_sb(data);
-		else if (ft_strcmp(line, "pa") == 0)
-			push_to_a(data);
-		else if (ft_strcmp(line, "pb") == 0)
-			push_to_b(data);
-		else if (ft_strcmp(line, "ra") == 0)
-			rotate_a(data, 1);
-		else if (ft_strcmp(line, "rb") == 0)
-			rotate_b(data, 1);
-		else if (ft_strcmp(line, "rr") == 0)
-			ra_rb(data);
-		else if (ft_strcmp(line, "rra") == 0)
-			r_rotate_a(data, 1);
-		else if (ft_strcmp(line, "rrb") == 0)
-			r_rotate_b(data, 1);
-		else if (ft_strcmp(line, "rrr") == 0)
-			rrr(data);
-		else if (ft_strcmp(line, "exit") == 0)
-			break ;
-		debug_ps(*data);
+	if (parser == true)
+	{ 
+		char	line[256];
+		while (1)
+		{
+			scanf("%255[^\n]%*c",line);
+			if (ft_strcmp(line, "sa") == 0)
+				swap_a(data, 1);
+			else if (ft_strcmp(line, "sb") == 0)
+				swap_b(data, 1);
+			else if (ft_strcmp(line, "ss") == 0)
+				sa_sb(data);
+			else if (ft_strcmp(line, "pa") == 0)
+				push_to_a(data);
+			else if (ft_strcmp(line, "pb") == 0)
+				push_to_b(data);
+			else if (ft_strcmp(line, "ra") == 0)
+				rotate_a(data, 1);
+			else if (ft_strcmp(line, "rb") == 0)
+				rotate_b(data, 1);
+			else if (ft_strcmp(line, "rr") == 0)
+				ra_rb(data);
+			else if (ft_strcmp(line, "rra") == 0)
+				r_rotate_a(data, 1);
+			else if (ft_strcmp(line, "rrb") == 0)
+				r_rotate_b(data, 1);
+			else if (ft_strcmp(line, "rrr") == 0)
+				rrr(data);
+			else if (ft_strcmp(line, "exit") == 0)
+				break ;
+			debug_ps(*data);
+		}
 	}
 	free_stack(data->a);
 	free_stack(data->b);
